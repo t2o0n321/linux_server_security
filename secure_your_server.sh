2783a899ffbd3a1776675dba4b972a6a0772f5dd
t2o0n321 <i915915514@icloud.com>
2025-06-04 10:04:04 +0800

    Fix

--- secure_your_server.sh
+++ secure_your_server.sh
@@ -280,7 +280,7 @@ setup_ufw() {
 
     # Enable UFW
     sudo ufw --force enable || error_exit "Failed to enable UFW"
-    log "INFO" "UFW configured with allowed ports: ${UFW_ALLOWED_PORTS[*]}"
+    log "INFO" "UFW configured with allowed ports: $(IFS=','; echo "${missing_options[*]}")"
 }
 
 # Install Fail2Ban
